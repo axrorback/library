bd1428e6286b090c7d667f0262c4410ea30d93db
axrorback <axacyber1@gmail.com>
2026-01-26 09:04:29 +0500

    Optimize change password view by refining imports

--- accounts/views/ChangePassword.py
+++ accounts/views/ChangePassword.py
@@ -1,16 +1,8 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import update_session_auth_hash
-from django.contrib.auth.forms import PasswordChangeForm
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-
-
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm, SetPasswordForm
 from django.contrib.auth import update_session_auth_hash
 from django.shortcuts import render, redirect
 from django.contrib import messages
-
 
 @login_required
 def change_password(request):
