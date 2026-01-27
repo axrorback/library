cc061e7819220dbba329b786bfd60b39cfc8bbb7
axrorback <axacyber1@gmail.com>
2026-01-27 13:16:14 +0500

    Integrate Telegram `send_message` utility in account utils for bot communication

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,5 +1,16 @@
 from random import randint
 from secrets import token_urlsafe
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')
+
+URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+
+def send_message(message,chat_id):
+    requests.post(URL,data={'chat_id':chat_id,'text':message})
 
 def generate_otp():
     return randint(111111,999999)
