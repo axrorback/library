1f6201c0bf1e79312c471356d0f3654c9d8c5027
axrorback <axrorback@gmail.com>
2026-05-01 09:15:20 +0500

    Fix missing slash in `create_book_payment` URL path definition

--- library/urls.py
+++ library/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("categories/<slug:category_slug>/", BookByCategoryListView.as_view(), name="book_list_by_category"),
     path("categories/<slug:category_slug>/<slug:book_slug>/", BookDetailView.as_view(), name="book_detail"),
     path('callback/',payment_callback,name='payment_callback_book'),
-    path('create-book-payment<uuid:id>/',create_book_payment,name='create_book_payment')
+    path('create-book-payment/<uuid:id>/',create_book_payment,name='create_book_payment')
 
 ]
