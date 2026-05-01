8bd0a729a2e88e00c4c3a8420d7b8d168be7e168
axrorback <axrorback@gmail.com>
2026-05-01 09:09:32 +0500

    Add `create_book_payment` view and URL for book payment creation

--- library/urls.py
+++ library/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", CategoryListView.as_view(), name="category_list"),
     path("categories/<slug:category_slug>/", BookByCategoryListView.as_view(), name="book_list_by_category"),
     path("categories/<slug:category_slug>/<slug:book_slug>/", BookDetailView.as_view(), name="book_detail"),
-    path('callback/',payment_callback,name='payment_callback_book')
+    path('callback/',payment_callback,name='payment_callback_book'),
+    path('create-book-payment/',create_book_payment,name='create_book_payment')
 
 ]
--- library/views/__init__.py
+++ library/views/__init__.py
@@ -2,3 +2,4 @@ from library.views.CategoryList import CategoryListView
 from library.views.BookList import BookByCategoryListView
 from library.views.BookDetail import BookDetailView
 from library.views.CallBackBookPayment import payment_callback
+from library.views.PayForBook import create_book_payment
